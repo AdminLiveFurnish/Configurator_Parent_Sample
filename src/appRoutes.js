11abc654c3d079cb09e2ade5b884c192a0645649
jaypal-tagline <123442567+jaypal-tagline@users.noreply.github.com>
2023-12-11 18:14:22 +0530

    Product configurator added for rockerz

--- src/appRoutes.js
+++ src/appRoutes.js
@@ -2,14 +2,20 @@ import { lazy, Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
 import Loader from './components/Loader/Loader';
-export const CONFIGURATOR_VIEWER = "/configurator/:modelId";
+// export const CONFIGURATOR_VIEWER = "/configurator/:modelId";
+export const CONFIGURATOR_VIEWER_INVERTED = "/configurator/1";
+export const CONFIGURATOR_VIEWER_QUAD = "/configurator/2";
+export const CONFIGURATOR_VIEWER_OCTO = "/configurator/3";
 export const MODEL_VIEWER = "/modelviewer/:modelId";
 export const MODEL_VIEWER_INVERTED = "/modelviewer/1";
 export const MODEL_VIEWER_QUAD = "/modelviewer/2";
 export const MODEL_VIEWER_OCTO = "/modelviewer/3";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
-const ConfiguratorViewer = lazy(() => import("./pages/ConfiguratorViewer"));
+// const ConfiguratorViewer = lazy(() => import("./pages/ConfiguratorViewer"));
+const ConfiguratorViewerInverted = lazy(() => import("./pages/ConfiguratorViewerInverted"));
+const ConfiguratorViewerQuad = lazy(() => import("./pages/ConfiguratorViewerQuad"));
+const ConfiguratorViewerOcto = lazy(() => import("./pages/ConfiguratorViewerOcto"));
 // const ModelViewer = lazy(() => import("./pages/ModelViewer"));
 const ModelViewerInverted = lazy(() => import("./pages/ModelViewerInverted"));
 const ModelViewerQuad = lazy(() => import("./pages/ModelViewerQuad"));
@@ -32,10 +38,10 @@ const routeMapFunc = (
 };
 
 export const routes = [
-  { path: CONFIGURATOR_VIEWER, Component: ConfiguratorViewer, key: "configurator" },
-  // { path: CONFIGURATOR_VIEWER, Component: ConfiguratorViewer, key: "configurator1" },
-  // { path: CONFIGURATOR_VIEWER, Component: ConfiguratorViewer, key: "configurato2" },
-  // { path: CONFIGURATOR_VIEWER, Component: ConfiguratorViewer, key: "configurator3" },
+  // { path: CONFIGURATOR_VIEWER, Component: ConfiguratorViewer, key: "configurator" },
+  { path: CONFIGURATOR_VIEWER_INVERTED, Component: ConfiguratorViewerInverted, key: "configurator1" },
+  { path: CONFIGURATOR_VIEWER_QUAD, Component: ConfiguratorViewerQuad, key: "configurato2" },
+  { path: CONFIGURATOR_VIEWER_OCTO, Component: ConfiguratorViewerOcto, key: "configurator3" },
   // { path: MODEL_VIEWER, Component: ModelViewer, key: "modelViewer" },
   { path: MODEL_VIEWER_INVERTED, Component: ModelViewerInverted, key: "modelViewer1" },
   { path: MODEL_VIEWER_QUAD, Component: ModelViewerQuad, key: "modelViewe2" },
